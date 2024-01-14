f4af65017f4765edaabc57c5e071bbd47f65f92b
alicalimli <alicalimli76@gmail.com>
2024-01-14 15:09:05 +0300

    newsletter section

---
+++ app/newsletter.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+export function Newsletter() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Ali’s Newsletter</CardTitle>
+        <CardDescription>
+          Join 1k+ developers getting tips, strategies, and resources that would
+          help you succeed in your web dev journey.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter className="flex justify-between gap-2">
+        <Input id="email" placeholder="Enter your email" />
+        <Button className="w-40">Join In</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,31 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Newsletter } from "./newsletter";
 
 export default function Home() {
   return (
-    <main className="flex max-w-4xl mx-auto min-h-screen flex-col items-center justify-between p-24">
-      <header>
+    <main className="max-w-2xl mx-auto min-h-screen p-4 py-24">
+      <header className="">
         <Avatar className="w-24 h-24">
           <AvatarImage src="/alicalimli.jpg" alt="Ali Calimli's Profile" />
           <AvatarFallback>AL</AvatarFallback>
         </Avatar>
 
-        <h2 className="text-3xl font-semibold tracking-tight">Ali Calimli</h2>
+        <h2 className="text-3xl font-semibold mt-2 tracking-tight">
+          Ali Calimli
+        </h2>
         <p className="text-xl text-white/80">
           Front-end Developer & Content Creator
         </p>
 
-        <p className="text-xl text-muted-foreground">
+        <p className="text-xl text-muted-foreground mt-3">
           Focused on building awesome and high-quality websites that are also
           visually appealing and easy to use.
         </p>
       </header>
+
+      <section className="mt-8">
+        <Newsletter />
+      </section>
     </main>
   );
 }
